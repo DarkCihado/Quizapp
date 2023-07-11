3a936eb6e72696ef8ae609c01a0f130f3c0077b4
Cihad Isen <chihad@live.de>
2023-07-11 21:35:41 +0200

    added button disable and enable

--- script.js
+++ script.js
@@ -67,17 +67,35 @@ let questions = [
 
 let currentQuestion = 0;
 
-function init(){
-    document.getElementById('all-questions').innerHTML = questions.length;
-    showQuestion();
+function init() {
+  document.getElementById("all-questions").innerHTML = questions.length;
+  showQuestion();
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
-    
+  let question = questions[currentQuestion];
+  document.getElementById("questiontext").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+function answer(selection) {
+  let question = questions[currentQuestion];
+
+  let selectedQuestionNumber = selection.slice(-1);
+
+  let idOfRightAnswer = `answer_${question["right_answer"]}`;
+
+  if (selectedQuestionNumber == question["right_answer"]) {
+    document.getElementById(selection).parentNode.classList.add("bg-success");
+  } else {
+    document.getElementById(selection).parentNode.classList.add("bg-danger");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
+  }
+
+  document.getElementById("next-button").disabled = false;
 }
